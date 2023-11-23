4adf15d6ca8ec21b03d515688002538ed561f885
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-11-23 15:21:33 +0100

    Append the children

--- src/DOM.js
+++ src/DOM.js
@@ -42,201 +42,234 @@ export function displayProject(project, todoList, projectId) {
     projectContainer.appendChild(projectHeader);
 
     function displayTodo(todoTitle, todoDescription, todoDueDate, todoPriority, todoStatus, todoId, todoExpandState) {
-    const todoItem = document.createElement('div');
-    todoItem.classList.add('todo');
+        const todoItem = document.createElement('div');
+        todoItem.classList.add('todo');
 
-    const unexpandedTodo = document.createElement('div');
-    unexpandedTodo.classList.add('unexpanded-todo');
+        const unexpandedTodo = document.createElement('div');
+        unexpandedTodo.classList.add('unexpanded-todo');
 
-    const unexpandedTodoButtons = document.createElement('div');
-    unexpandedTodoButtons.classList.add('unexpanded-todo-buttons');
+        const unexpandedTodoButtons = document.createElement('div');
+        unexpandedTodoButtons.classList.add('unexpanded-todo-buttons');
 
-    const titleContainer = document.createElement('div');
-    titleContainer.classList.add('title-container');
+        const titleContainer = document.createElement('div');
+        titleContainer.classList.add('title-container');
 
-    const titleHeader = document.createElement('p');
-    titleHeader.classList.add('title-header');
-    titleHeader.textContent = 'Title';
+        const titleHeader = document.createElement('p');
+        titleHeader.classList.add('title-header');
+        titleHeader.textContent = 'Title';
 
-    const title = document.createElement('p');
-    title.classList.add('todo-title');
-    title.contentEditable = true;
-    title.setAttribute('data-project-id', projectId);
-    title.setAttribute('data-id', todoId);
-    title.textContent = todoTitle;
+        const title = document.createElement('p');
+        title.classList.add('todo-title');
+        title.contentEditable = true;
+        title.setAttribute('data-project-id', projectId);
+        title.setAttribute('data-id', todoId);
+        title.textContent = todoTitle;
 
-    const deleteButton = document.createElement('span');
-    deleteButton.classList.add('material-symbols-outlined', 'delete-button', 'delete-buttons');
-    deleteButton.textContent = 'delete';
-    deleteButton.setAttribute('data-project-id', projectId);
-    deleteButton.setAttribute('data-id', todoId);
+        const deleteButton = document.createElement('span');
+        deleteButton.classList.add('material-symbols-outlined', 'delete-button', 'delete-buttons');
+        deleteButton.textContent = 'delete';
+        deleteButton.setAttribute('data-project-id', projectId);
+        deleteButton.setAttribute('data-id', todoId);
 
-    const checkBox = document.createElement('span');
-    checkBox.classList.add('material-symbols-outlined', 'check-box');
-    checkBox.setAttribute('data-project-id', projectId);
-    checkBox.setAttribute('data-id', todoId);
+        const checkBox = document.createElement('span');
+        checkBox.classList.add('material-symbols-outlined', 'check-box');
+        checkBox.setAttribute('data-project-id', projectId);
+        checkBox.setAttribute('data-id', todoId);
 
-    const priorityIconContainer = document.createElement('div');
-    priorityIconContainer.classList.add('priority-icon-container');
+        const priorityIconContainer = document.createElement('div');
+        priorityIconContainer.classList.add('priority-icon-container');
 
-    //Define styles based on the current status and priority of a todo
-    if (todoStatus === 'Done') {
-        checkBox.textContent = 'check_box';
-        title.style.textDecoration = 'line-through';
-        priorityIconContainer.style.backgroundColor = 'lightgreen';
-        checkBox.style.color = 'mediumseagreen';
-    } else {
-        checkBox.textContent = 'check_box_outline_blank';
+        //Define styles based on the current status and priority of a todo
+        if (todoStatus === 'Done') {
+            checkBox.textContent = 'check_box';
+            title.style.textDecoration = 'line-through';
+            priorityIconContainer.style.backgroundColor = 'lightgreen';
+            checkBox.style.color = 'mediumseagreen';
+        } else {
+            checkBox.textContent = 'check_box_outline_blank';
 
-        if (todoPriority === 'High') {
-        priorityIconContainer.style.backgroundColor = '';
-        priorityIconContainer.textContent = '🔥';
+            if (todoPriority === 'High') {
+            priorityIconContainer.style.backgroundColor = '';
+            priorityIconContainer.textContent = '🔥';
+            }
         }
-    }
 
-    //An expanded todo
-    const expandedTodo = document.createElement('div');
-    expandedTodo.classList.add('expanded-todo');
 
-    const descriptionContainer = document.createElement('div');
-    descriptionContainer.classList.add('description-container');
+        //An expanded todo
+        const expandedTodo = document.createElement('div');
+        expandedTodo.classList.add('expanded-todo');
 
-    const descriptionTitle = document.createElement('p');
-    descriptionTitle.classList.add('description-title');
-    descriptionTitle.textContent = 'Description';
+        const descriptionContainer = document.createElement('div');
+        descriptionContainer.classList.add('description-container');
 
-    const description = document.createElement('p');
-    description.classList.add('todo-description');
-    description.setAttribute('data-project-id', projectId);
-    description.setAttribute('data-id', todoId);
-    description.textContent = todoDescription;
-    description.contentEditable = true;
+        const descriptionTitle = document.createElement('p');
+        descriptionTitle.classList.add('description-title');
+        descriptionTitle.textContent = 'Description';
 
-    const dueDateContainer = document.createElement('div');
-    dueDateContainer.classList.add('due-date-container');
+        const description = document.createElement('p');
+        description.classList.add('todo-description');
+        description.setAttribute('data-project-id', projectId);
+        description.setAttribute('data-id', todoId);
+        description.textContent = todoDescription;
+        description.contentEditable = true;
 
-    const dueDateTitle = document.createElement('p');
-    dueDateTitle.classList.add('due-date-title');
-    dueDateTitle.textContent = 'Due Date';
+        const dueDateContainer = document.createElement('div');
+        dueDateContainer.classList.add('due-date-container');
 
-    const dueDate = document.createElement('p');
-    dueDate.classList.add('todo-due-date');
-    dueDate.setAttribute('data-project-id', projectId);
-    dueDate.setAttribute('data-id', todoId);
-    dueDate.textContent = todoDueDate;
-    dueDate.contentEditable = true;
+        const dueDateTitle = document.createElement('p');
+        dueDateTitle.classList.add('due-date-title');
+        dueDateTitle.textContent = 'Due Date';
 
-    //Display the remaining or overdue time based on the due date
-    const remainingTimeContainer = document.createElement('div');
-    remainingTimeContainer.style.display = 'none';
-    remainingTimeContainer.classList.add('remaining-time-container');
-    const remainingTimeTitle = document.createElement('div');
-    remainingTimeTitle.classList.add('remaining-time-title');
+        const dueDate = document.createElement('p');
+        dueDate.classList.add('todo-due-date');
+        dueDate.setAttribute('data-project-id', projectId);
+        dueDate.setAttribute('data-id', todoId);
+        dueDate.textContent = todoDueDate;
+        dueDate.contentEditable = true;
 
-    const targetDate = todoDueDate;
-    const potentialFormats = ['MM/dd/yyyy', 'yyyy-MM-dd', 'dd/MM/yyyy', 'yyyyMMdd', 'ddMMyyyy', 'MMddyyyy', 'yyyy MM dd', 'MM dd yyyy', 'dd MM yyyy'];
-    
-    let parsedDate;
-    for (const format of potentialFormats) {
-        parsedDate = parse(targetDate, format, new Date());
+        //Display the remaining or overdue time based on the due date
+        const remainingTimeContainer = document.createElement('div');
+        remainingTimeContainer.style.display = 'none';
+        remainingTimeContainer.classList.add('remaining-time-container');
+        const remainingTimeTitle = document.createElement('div');
+        remainingTimeTitle.classList.add('remaining-time-title');
+
+        const targetDate = todoDueDate;
+        const potentialFormats = ['MM/dd/yyyy', 'yyyy-MM-dd', 'dd/MM/yyyy', 'yyyyMMdd', 'ddMMyyyy', 'MMddyyyy', 'yyyy MM dd', 'MM dd yyyy', 'dd MM yyyy'];
+        
+        let parsedDate;
+        for (const format of potentialFormats) {
+            parsedDate = parse(targetDate, format, new Date());
+
+            if (!isNaN(parsedDate.getTime())) {
+                break;
+            }
+        }
 
         if (!isNaN(parsedDate.getTime())) {
-            break;
-        }
-    }
+            const currentDate = new Date();
+            const daysDifference = differenceInDays(parsedDate, currentDate);
 
-    if (!isNaN(parsedDate.getTime())) {
-        const currentDate = new Date();
-        const daysDifference = differenceInDays(parsedDate, currentDate);
+            const remainingTime = document.createElement('p');
+            remainingTime.classList.add('remaining-time');
 
-        const remainingTime = document.createElement('p');
-        remainingTime.classList.add('remaining-time');
-
-        if (todoStatus === 'Done') {
-            remainingTime.textContent = '';
-            remainingTimeTitle.textContent = '';
-        } else {
-            if (daysDifference >= 0) {
-                remainingTime.textContent = daysDifference;
-                remainingTimeTitle.textContent = 'Days Remaining';
-                remainingTimeContainer.appendChild(remainingTimeTitle);
-                remainingTimeContainer.appendChild(remainingTime);
-                remainingTimeContainer.style.display = 'block';
+            if (todoStatus === 'Done') {
+                remainingTime.textContent = '';
+                remainingTimeTitle.textContent = '';
             } else {
-                remainingTime.textContent = Math.abs(daysDifference);
-                remainingTimeTitle.textContent = 'Overdue Days 😭';
-                remainingTimeContainer.appendChild(remainingTimeTitle);
-                remainingTimeContainer.appendChild(remainingTime);
-                remainingTimeContainer.style.display = 'block';
+                if (daysDifference >= 0) {
+                    remainingTime.textContent = daysDifference;
+                    remainingTimeTitle.textContent = 'Days Remaining';
+                    remainingTimeContainer.appendChild(remainingTimeTitle);
+                    remainingTimeContainer.appendChild(remainingTime);
+                    remainingTimeContainer.style.display = 'block';
+                } else {
+                    remainingTime.textContent = Math.abs(daysDifference);
+                    remainingTimeTitle.textContent = 'Overdue Days 😭';
+                    remainingTimeContainer.appendChild(remainingTimeTitle);
+                    remainingTimeContainer.appendChild(remainingTime);
+                    remainingTimeContainer.style.display = 'block';
+                }
+
+
             }
-
-
+        } else {
+            remainingTimeContainer.style.display = 'none';
         }
-    } else {
-        remainingTimeContainer.style.display = 'none';
+
+
+        const priorityParentContainer = document.createElement('div');
+        priorityParentContainer.classList.add('priority-parent-container');
+
+        const priorityContainer = document.createElement('div');
+        priorityContainer.classList.add('priority-container');
+
+        const priorityTitle = document.createElement('p');
+        priorityTitle.classList.add('priority-title');
+        priorityTitle.textContent = 'Priority'
+
+        const priorityText = document.createElement('p');
+        priorityText.classList.add('priority-text');
+        priorityText.textContent = todoPriority;
+
+        const priorityButtons = document.createElement('div');
+        priorityButtons.classList.add('priority-buttons-container');
+
+        const lowPriority = document.createElement('button');
+        lowPriority.classList.add('low-priority-button', 'priority-buttons');
+        lowPriority.setAttribute('data-project-id', projectId);
+        lowPriority.setAttribute('data-id', todoId);
+        lowPriority.textContent = 'Low';
+
+        const highPriority = document.createElement('button');
+        highPriority.classList.add('high-priority-button', 'priority-buttons');
+        highPriority.setAttribute('data-project-id', projectId);
+        highPriority.setAttribute('data-id', todoId);
+        highPriority.textContent = 'High';
+
+        //Style priority buttons based on a priority
+        if (todoPriority === 'High') {
+            highPriority.classList.add('selected-priority');
+            lowPriority.classList.remove('selected-priority');
+        } else if (todoPriority === 'Low') {
+            highPriority.classList.remove('selected-priority');
+            lowPriority.classList.add('selected-priority');
+        }
+
+        const expandButtonContainer = document.createElement('div');
+        expandButtonContainer.classList.add('expand-button-container');
+
+        const expandButton = document.createElement('span');
+        expandButton.classList.add('material-symbols-outlined', 'expand-button');
+        expandButton.setAttribute('data-project-id', projectId);
+        expandButton.setAttribute('data-id', todoId);
+        expandButton.textContent = 'expand_more';
+
+        const expandLessButton = document.createElement('span')
+        expandLessButton.classList.add('material-symbols-outlined', 'expand-less-button');
+        expandLessButton.setAttribute('data-project-id', projectId);
+        expandLessButton.setAttribute('data-id', todoId);
+        expandLessButton.textContent = 'expand_less';
+
+        //Handle appearance of an expanded todo and change the button accordingly
+        if (todoExpandState === false) {
+            expandedTodo.style.display = 'none';
+            expandButtonContainer.appendChild(expandButton);
+        } else {
+            expandedTodo.style.display = 'flex';
+            expandButtonContainer.appendChild(expandLessButton);
+        }
+
+        //Append children
+        unexpandedTodoButtons.appendChild(priorityIconContainer);
+        unexpandedTodoButtons.appendChild(expandButtonContainer);
+        unexpandedTodoButtons.appendChild(checkBox);
+        unexpandedTodoButtons.appendChild(deleteButton);
+        titleContainer.appendChild(titleHeader);
+        titleContainer.appendChild(title);
+        unexpandedTodo.appendChild(titleContainer);
+        unexpandedTodo.appendChild(unexpandedTodoButtons);
+
+        priorityContainer.appendChild(priorityText);
+        priorityButtons.appendChild(lowPriority);
+        priorityButtons.appendChild(highPriority);
+        priorityContainer.appendChild(priorityButtons);
+        priorityParentContainer.appendChild(priorityTitle);
+        priorityParentContainer.appendChild(priorityContainer);
+        descriptionContainer.appendChild(descriptionTitle);
+        descriptionContainer.appendChild(description);
+        dueDateContainer.appendChild(dueDateTitle);
+        dueDateContainer.appendChild(dueDate);
+        expandedTodo.appendChild(descriptionContainer);
+        expandedTodo.appendChild(dueDateContainer);
+        expandedTodo.appendChild(remainingTimeContainer);
+        expandedTodo.appendChild(priorityParentContainer);
+
+        todoItem.appendChild(unexpandedTodo);
+        todoItem.appendChild(expandedTodo);
+
+        projectContainer.appendChild(todoItem);
     }
 
-    const priorityParentContainer = document.createElement('div');
-    priorityParentContainer.classList.add('priority-parent-container');
-
-    const priorityContainer = document.createElement('div');
-    priorityContainer.classList.add('priority-container');
-
-    const priorityTitle = document.createElement('p');
-    priorityTitle.classList.add('priority-title');
-    priorityTitle.textContent = 'Priority'
-
-    const priorityText = document.createElement('p');
-    priorityText.classList.add('priority-text');
-    priorityText.textContent = todoPriority;
-
-    const priorityButtons = document.createElement('div');
-    priorityButtons.classList.add('priority-buttons-container');
-
-    const lowPriority = document.createElement('button');
-    lowPriority.classList.add('low-priority-button', 'priority-buttons');
-    lowPriority.setAttribute('data-project-id', projectId);
-    lowPriority.setAttribute('data-id', todoId);
-    lowPriority.textContent = 'Low';
-
-    const highPriority = document.createElement('button');
-    highPriority.classList.add('high-priority-button', 'priority-buttons');
-    highPriority.setAttribute('data-project-id', projectId);
-    highPriority.setAttribute('data-id', todoId);
-    highPriority.textContent = 'High';
-
-    //Style priority buttons based on a priority
-    if (todoPriority === 'High') {
-        highPriority.classList.add('selected-priority');
-        lowPriority.classList.remove('selected-priority');
-    } else if (todoPriority === 'Low') {
-        highPriority.classList.remove('selected-priority');
-        lowPriority.classList.add('selected-priority');
-    }
-
-    const expandButtonContainer = document.createElement('div');
-    expandButtonContainer.classList.add('expand-button-container');
-
-    const expandButton = document.createElement('span');
-    expandButton.classList.add('material-symbols-outlined', 'expand-button');
-    expandButton.setAttribute('data-project-id', projectId);
-    expandButton.setAttribute('data-id', todoId);
-    expandButton.textContent = 'expand_more';
-
-    const expandLessButton = document.createElement('span')
-    expandLessButton.classList.add('material-symbols-outlined', 'expand-less-button');
-    expandLessButton.setAttribute('data-project-id', projectId);
-    expandLessButton.setAttribute('data-id', todoId);
-    expandLessButton.textContent = 'expand_less';
-
-    //Handle appearance of an expanded todo and change the button accordingly
-    if (todoExpandState === false) {
-        expandedTodo.style.display = 'none';
-        expandButtonContainer.appendChild(expandButton);
-    } else {
-        expandedTodo.style.display = 'flex';
-        expandButtonContainer.appendChild(expandLessButton);
-    }
-
-    }
+    
 }
