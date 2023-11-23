65a1b4303584253ce9f1b393aa0a2eccc143eae6
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-11-23 14:57:32 +0100

    Handle a project and a todo title change

---
+++ src/handleInput.js
@@ -0,0 +1,29 @@
+import { projects, storeProjects } from './storage';
+
+export function handleProjectTitleChange(title) {
+    const projectId = title.getAttribute('data-project-id');
+    projects.forEach(project => {
+            if (projectId === project.id) {
+                project.changeProjectTitle(title.innerText);
+                console.log(project);
+            }
+        });
+    storeProjects();
+}
+
+export function handleTitleChange(title) {
+    const projectId = title.getAttribute('data-project-id');
+    const titleId = title.getAttribute('data-id');
+    
+    projects.forEach(project => {
+        if (projectId === project.id) {
+            project.todoList.forEach(todo => {
+                if (todo.id === titleId) {
+                    todo.changeTitle(title.innerText);
+                    console.log(project);
+                }
+            })
+        }
+    });
+    storeProjects();
+}
