7819473711ca6e92deb31cfa994112528e14172b
Hamza Mujinovic <mujinovichamza@gmail.com>
2023-11-23 14:44:36 +0100

    Create the Todo and RetrievedTodo Classes

--- src/createProject.js
+++ src/createProject.js
@@ -17,7 +17,6 @@ export class Project {
     }
 }
 
-
 export class RetrievedProject {
     constructor(title, todoList, id) {
         this.title = title;
@@ -31,5 +30,102 @@ export class RetrievedProject {
 
     changeProjectTitle(newTitle) {
         this.title = newTitle;
+    }
+}
+
+
+export default class Todo {
+    constructor(title, description, dueDate, priority, status) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = status;
+        this.id = uniqueId();
+        this.expanded = false;
+    }
+
+    changeTitle(string) {
+        this.title = string;
+    }
+
+    changeDescription(string) {
+        this.description = string;
+    }
+
+    changeDueDate(string) {
+        this.dueDate = string;
+    }
+
+    changePriorityToLow() {
+        this.priority = 'Low';
+    }
+
+    changePriorityToHigh() {
+        this.priority = 'High';
+    }
+
+    changeStatusToDone() {
+        this.status = 'Done';
+    }
+
+    changeStatusToInProgress() {
+        this.status = 'In progress';
+    }
+
+    changeExpandedToTrue() {
+        this.expanded = true;
+    }
+
+    changeExpandedToFalse() {
+        this.expanded = false;
+    }
+}
+
+export class RetrievedTodo {
+    constructor(title, description, dueDate, priority, status, id, expanded) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = status;
+        this.id = id;
+        this.expanded = expanded;
+    }
+
+    changeTitle(string) {
+        this.title = string;
+    }
+
+    changeDescription(string) {
+        this.description = string;
+    }
+
+    changeDueDate(string) {
+        this.dueDate = string;
+    }
+
+    changePriorityToLow() {
+        this.priority = 'Low';
+    }
+
+    changePriorityToHigh() {
+        this.priority = 'High';
+    }
+
+    changeStatusToDone() {
+        this.status = 'Done';
+    }
+
+    changeStatusToInProgress() {
+        this.status = 'In progress';
+    }
+
+    changeExpandedToTrue() {
+        this.expanded = true;
+    }
+
+    changeExpandedToFalse() {
+        this.expanded = false;
     }
 }
